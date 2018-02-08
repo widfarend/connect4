039b982dc1540dd0b7057bf10911f4bb6d16ff7d
Anthony <anthony.williams@fluidads.com>
2018-02-08 12:01:27 +0000

    Added comments, win conditions and another computer player type

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const Game = require('./lib/game');
+
+// Players 1 and 2 can be both human, one human and one computer or both computer players
+// Computer players are:
+// 'computer_dumb' -- random counter placement
+// 'computer_linear' -- linear counter placement
 const player1 = {name: 'Player 1', counterIcon: '*', type: 'human'};
-const player2 = {name: 'Computer', counterIcon: '@', type: 'computer_dumb'};
-const boardSize = { cols: 7, rows: 6};
+const player2 = {name: 'Computer', counterIcon: '@', type: 'human'};
+const boardConf = { cols: 7, rows: 6, maxCountToWin: 4};
 
-let connect4 = new Game(player1, player2, boardSize);
-
+let connect4 = new Game(player1, player2, boardConf);
 connect4.run();
 
