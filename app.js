f4df772dbdc945928f4f7b57376d666882cc8edf
Anthony Williams <anthony@Anthonys-MacBook-Pro.local>
2018-02-08 01:00:41 +0000

    Added dumb AI (including a fake thinking animation!), user input, setup a win condition stub and cleaned up some of the console logging.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Game = require('./lib/game');
 const player1 = {name: 'Player 1', counterIcon: '*', type: 'human'};
-const player2 = {name: 'Computer', counterIcon: '+', type: 'computer_dumb'};
+const player2 = {name: 'Computer', counterIcon: '@', type: 'computer_dumb'};
 const boardSize = { cols: 7, rows: 6};
 
 let connect4 = new Game(player1, player2, boardSize);
